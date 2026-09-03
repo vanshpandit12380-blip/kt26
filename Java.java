de6a6cc0cc631f2d91f6e6f60b182638b384714e
vansh <vanshpandit12380@gmail.com>
2026-09-04 00:21:23 +0530

    Initial commit

---
+++ Java.java
@@ -0,0 +1,13 @@
+public class Vansh_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Vansh!");
+        System.out.println("B.Tech CSE - First Year");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
